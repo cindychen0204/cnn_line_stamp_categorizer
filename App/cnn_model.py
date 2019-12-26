ed8ccc129c1d1f0ff2c90ceed053b1b6b56ce905
Cindy Chen <cindychen0204@hololab.co.jp>
2019-12-26 22:57:50 +0900

    名前重複しないスクリプトの変更と他のリファクタリング

--- App/cnn_model.py
+++ App/cnn_model.py
@@ -1,8 +1,8 @@
 import keras
-from keras.models import  Sequential
+from keras.models import Sequential
 from keras.layers import Dense,Dropout,Flatten
 from keras.layers import Conv2D,MaxPooling2D
-from keras.optimizers import  RMSprop
+from keras.optimizers import RMSprop
 
 def Define_CNNModel(in_shape,nb_classes):
     model=Sequential()
--- App/rename.py
+++ App/rename.py
@@ -1,13 +1,19 @@
 import glob
-import os
+import os.path
+from os import path
 
 line_stamps_path = "../line_stamps/"
 categories = os.listdir(line_stamps_path)
+#print(len(categories))
 
 for category in categories:
     files = glob.glob(line_stamps_path + category + "/*.png")
+    exitedName = []
     for i, old_fileName in enumerate(files):
-        print(old_fileName)
+        exitedName.append(old_fileName)
+        #print(old_fileName)
         new_name = os.path.join(line_stamps_path + category, '{0:03d}'.format(i) + ".png")
+        if(path.exists(new_name)):
+            new_name = os.path.join(line_stamps_path + category, '{0:04d}'.format(i) + ".png")
         os.rename(old_fileName, new_name)
-        print("Change Name from " + old_fileName + " -> " + new_name)
+    print(category +" : " + str(len(files)))
